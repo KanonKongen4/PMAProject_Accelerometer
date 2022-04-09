57e052a8072805af34532498d8698096831847cd
KanonKongen4 <91434226+KanonKongen4@users.noreply.github.com>
2022-04-09 11:33:02 +0200

    Added Accelerometer test code

--- app/src/main/java/com/example/pma_project_accelerometer/MainActivity.java
+++ app/src/main/java/com/example/pma_project_accelerometer/MainActivity.java
@@ -2,13 +2,59 @@ package com.example.pma_project_accelerometer;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
+import android.os.Looper;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.logging.Handler;
 
 public class MainActivity extends AppCompatActivity {
+
+    private SensorManager sensorManager;
+    private Sensor accelerometer;
+
+    private TextView messageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        Sensor sensor = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        sensorManager.registerListener(listener, sensor, SensorManager.SENSOR_DELAY_NORMAL);
+    messageView = findViewById(R.id.messageView);
+        messageView.setText(":C!");
     }
+    private SensorEventListener listener = new SensorEventListener() {
+        @Override
+        public void onSensorChanged(SensorEvent event) {
+// The acceleration may be negative, so take their absolute value
+            float xValue = Math.abs(event.values[0]);
+            float yValue = Math.abs(event.values[1]);
+            float zValue = Math.abs(event.values[2]);
+            if (xValue > 10 || yValue > 10|| zValue > 10) {
+// message for user
+            messageView.setText("wuhuuu");
+
+            }
+        }
+        @Override
+        public void onAccuracyChanged(Sensor sensor, int accuracy) {
+        }
+    };
+    final Handler handler = new Handler(Looper.getMainLooper());
+handler.postDelayed(new Runnable() {
+        @Override
+        public void run() {
+            //Do something after 100ms
+        }
+    }, 100);
 }
