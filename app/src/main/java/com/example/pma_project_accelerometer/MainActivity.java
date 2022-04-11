32fb2c596e094a5f696bc01c8f47b8db9f093215
Julsgaard <kjulsg21@student.aau.dk>
2022-04-11 11:51:31 +0200

    Main stuff

--- app/src/main/java/com/example/pma_project_accelerometer/MainActivity.java
+++ app/src/main/java/com/example/pma_project_accelerometer/MainActivity.java
@@ -11,4 +11,30 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+<<<<<<< Updated upstream
+=======
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int i) {
+
+    }
+
+    @Override
+    protected void onResume(){
+        super.onResume();
+        if(mSensorManager.getDefaultSensor(Sensor.TYPE_STEP_COUNTER)!= null){
+            mSensorManager.registerListener(this,mySteps,SensorManager.SENSOR_DELAY_FASTEST);
+        }
+
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        if(mSensorManager.getDefaultSensor(Sensor.TYPE_STEP_COUNTER)!= null){
+            mSensorManager.unregisterListener(this,mySteps);
+        }
+    }
+
+>>>>>>> Stashed changes
 }
